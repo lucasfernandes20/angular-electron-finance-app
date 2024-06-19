682fb4e5a287490c6072915a31a9ea1a6a8794ea
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2024-06-19 17:47:24 -0300

    feat: create route provider

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TasksComponent } from './pages/tasks/tasks.component';
+import { ConfigComponent } from './pages/config/config.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: TasksComponent},
+  {path: 'config', component: ConfigComponent},
+];
